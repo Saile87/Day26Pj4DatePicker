84bf0be14f67ee22212181cc269b3d85b0214656
Elias Breitenbach <elias.b@me.com>
2023-04-11 18:58:03 +0200

    Create a Project

---
+++ Day25pj4_DatePicker/Day25pj4_DatePicker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Day25pj4_DatePicker
+//
+//  Created by Elias Breitenbach on 11.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day25pj4_DatePicker/Day25pj4_DatePicker/Day25pj4_DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day25pj4_DatePickerApp.swift
+//  Day25pj4_DatePicker
+//
+//  Created by Elias Breitenbach on 11.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Day25pj4_DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
